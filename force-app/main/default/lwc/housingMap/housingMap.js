a77857574bc36f64fb7f83a97fa9438f64b34f49
taishi0502 <kimura.taishi@robotpayment.co.jp>
2023-10-08 17:33:52 +0900

    lwcコンポ作成

--- force-app/main/default/lwc/housingMap/housingMap.js
+++ force-app/main/default/lwc/housingMap/housingMap.js
@@ -6,7 +6,21 @@ export default class HousingMap extends LightningElement {
     @wire(getHouses)
     wiredHouses({ error, data }) {
         if (data) {
-        console.log(data);
+        // We are using Javascript Map function to transform the
+    this.mapMarkers = data.map((element) => {
+      return {
+        location: {
+          Street: element.Address__c,
+          City: element.City__c,
+          State: element.State__c
+        },
+        title: element.Name
+      };
+    });
+    this.error = undefined;
+  } else if (error) {
+    this.error = error;
+    this.mapMarkers = undefined;
     }
   }
 }
